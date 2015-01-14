c691e2c2861116929cebade5196730693843f729
Valentin <valentin.lievin@gmail.com>
2015-01-14 20:27:59 +0100

    création du projet OutilsBillard : réécriture du code du projet BoiteAOutilBillard avec écriture des tests

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/**
+*
+*   \file main.cpp
+*   \brief fonction principale du bloc calcul de trajectoire du projet "Tricher au billard Français" pour l'ECN;
+*   \author Valentin LIEVIN
+*   \date 14 janvier 2015
+*
+**/
+
+
+#include<opencv2/highgui/highgui.hpp>
+
+
+
+int main(){
+
+cvNamedWindow("OutilsBillard",CV_WINDOW_AUTOSIZE);
+
+cvWaitKey(0);
+
+
+}
+
